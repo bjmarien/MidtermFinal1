c869a69a5fc7938a5282579b277629d0ab5e903b
bjmarien <31636098+bjmarien@users.noreply.github.com>
2017-10-17 16:31:38 -0400

    another commit

--- Midterm Coding Project/src/main/java/com/cisc181/core/Staff.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Staff.java
@@ -8,25 +8,25 @@ public class Staff extends Employee {
 
 	 private eTitle Title;
 
-	   public Staff(eTitle title){
+	   public Staff(eTitle title)
+	   {
 	        this.Title = title;
 	    }
 
-	    public void setTitle(eTitle title){
+	    public void setTitle(eTitle title)
+	    {
 	        this.Title = title;
 	    }
-	    public eTitle getTitle(){
+	    
+	    public eTitle getTitle()
+	    {
 	        return this.Title;
 	    }
 
-    
-
-    
 	public Staff(String FirstName, String MiddleName, String LastName,
 			Date DOB, String Address, String Phone_number, String Email,
 			String officeHours, int rank, double salary, Date hireDate,
-			eTitle Title) 
-	
+			eTitle Title) throws PersonException 
 	{
 		super(FirstName, MiddleName, LastName, DOB, Address, Phone_number,
 				Email, officeHours, salary, hireDate);
